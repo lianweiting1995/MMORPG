b33bb8cc5ac7d1bc750af7675c40a7156b2736dc
lianweiting <lianweiting1995@gmail.com>
2025-08-04 16:55:47 +0800

    网关新增服务发现

--- app/gateway/cmd/gateway/main.go
+++ app/gateway/cmd/gateway/main.go
@@ -6,6 +6,7 @@ import (
 
 	"MMORPG/app/gateway/internal/conf"
 
+	consul "github.com/go-kratos/kratos/contrib/registry/consul/v2"
 	"github.com/go-kratos/kratos/v2"
 	"github.com/go-kratos/kratos/v2/config"
 	"github.com/go-kratos/kratos/v2/config/file"
@@ -33,7 +34,7 @@ func init() {
 	flag.StringVar(&flagconf, "conf", "../../configs", "config path, eg: -conf config.yaml")
 }
 
-func newApp(logger log.Logger, gs *grpc.Server, hs *http.Server) *kratos.App {
+func newApp(logger log.Logger, gs *grpc.Server, hs *http.Server, cr *consul.Registry) *kratos.App {
 	return kratos.New(
 		kratos.ID(id),
 		kratos.Name(Name),
@@ -44,6 +45,7 @@ func newApp(logger log.Logger, gs *grpc.Server, hs *http.Server) *kratos.App {
 			gs,
 			hs,
 		),
+		kratos.Registrar(cr),
 	)
 }
 
@@ -74,7 +76,7 @@ func main() {
 		panic(err)
 	}
 
-	app, cleanup, err := wireApp(bc.Server, bc.Data, logger)
+	app, cleanup, err := wireApp(bc.Server, bc.Registry, bc.Data, logger)
 	if err != nil {
 		panic(err)
 	}
---
+++ app/gateway/internal/server/consul.go
@@ -0,0 +1,21 @@
+package server
+
+import (
+	"MMORPG/app/gateway/internal/conf"
+
+	consul "github.com/go-kratos/kratos/contrib/registry/consul/v2"
+
+	"github.com/go-kratos/kratos/v2/log"
+	"github.com/hashicorp/consul/api"
+)
+
+func NewConsul(c *conf.Registry, logger log.Logger) *consul.Registry {
+	config := api.DefaultConfig()
+	config.Address = c.Consul.Addrs[0]
+	client, err := api.NewClient(config)
+	if err != nil {
+		panic(err)
+	}
+
+	return consul.New(client)
+}
