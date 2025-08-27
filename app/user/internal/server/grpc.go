4ab1265762e2dc97893b1a70a2884de240311232
lianweiting <lianweiting1995@gmail.com>
2025-08-27 16:32:43 +0800

    配置调整

--- app/user/internal/server/grpc.go
+++ app/user/internal/server/grpc.go
@@ -5,6 +5,7 @@ import (
 	user_v1 "MMORPG/api/user/v1"
 	"MMORPG/app/user/internal/conf"
 	"MMORPG/app/user/internal/service"
+	"os"
 
 	"github.com/go-kratos/kratos/v2/log"
 	"github.com/go-kratos/kratos/v2/middleware/ratelimit"
@@ -25,7 +26,10 @@ func NewGRPCServer(c *conf.Server, greeter *service.GreeterService, user *servic
 	if c.Grpc.Network != "" {
 		opts = append(opts, grpc.Network(c.Grpc.Network))
 	}
-	if c.Grpc.Addr != "" {
+	addr := os.Getenv("GRPC_ADDR")
+	if addr != "" {
+		opts = append(opts, grpc.Address(addr))
+	} else if c.Grpc.Addr != "" {
 		opts = append(opts, grpc.Address(c.Grpc.Addr))
 	}
 	if c.Grpc.Timeout != nil {
--- app/user/internal/server/http.go
+++ app/user/internal/server/http.go
@@ -5,6 +5,7 @@ import (
 	user_v1 "MMORPG/api/user/v1"
 	"MMORPG/app/user/internal/conf"
 	"MMORPG/app/user/internal/service"
+	"os"
 
 	"github.com/go-kratos/kratos/v2/log"
 	"github.com/go-kratos/kratos/v2/middleware/recovery"
@@ -21,7 +22,9 @@ func NewHTTPServer(c *conf.Server, greeter *service.GreeterService, user *servic
 	if c.Http.Network != "" {
 		opts = append(opts, http.Network(c.Http.Network))
 	}
-	if c.Http.Addr != "" {
+	if addr := os.Getenv("HTTP_ADDR"); addr != "" {
+		opts = append(opts, http.Address(addr))
+	} else if c.Http.Addr != "" {
 		opts = append(opts, http.Address(c.Http.Addr))
 	}
 	if c.Http.Timeout != nil {
